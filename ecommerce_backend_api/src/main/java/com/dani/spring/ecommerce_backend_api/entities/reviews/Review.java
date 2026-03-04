b230ed7250ac1d29ad53a44adf7eef5241a6c429
dani <danii_v_m2001@hotmail.com>
2026-03-04 11:30:48 +0100

    feat: creación entity Review

---
+++ ecommerce_backend_api/src/main/java/com/dani/spring/ecommerce_backend_api/entities/reviews/Review.java
@@ -0,0 +1,108 @@
+package com.dani.spring.ecommerce_backend_api.entities.reviews;
+
+import com.dani.spring.ecommerce_backend_api.entities.product.Product;
+import com.dani.spring.ecommerce_backend_api.entities.user.User;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Table(name="reviews")
+@Entity
+public class Review {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    private Integer rating;
+
+    private String title;
+
+    private String comment;
+
+    private boolean purchased;
+
+    public Review(){
+    }
+
+    public Review(User user, Product product, Integer rating, String title, String comment, boolean purchased) {
+        this.user = user;
+        this.product = product;
+        this.rating = rating;
+        this.title = title;
+        this.comment = comment;
+        this.purchased = purchased;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public Integer getRating() {
+        return rating;
+    }
+
+    public void setRating(Integer rating) {
+        this.rating = rating;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public boolean isPurchased() {
+        return purchased;
+    }
+
+    public void setPurchased(boolean purchased) {
+        this.purchased = purchased;
+    }
+
+    
+
+}
